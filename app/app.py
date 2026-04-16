7e9418ca681d8fe0ed3e525074689413c944f17b
Alan <alananduaga07@gmail.com>
2026-04-16 17:21:51 -0600

    sintaxis de docker compose en pipeline

--- app/app.py
+++ app/app.py
@@ -8,5 +8,5 @@ def hello_world():
     return '¡Despliegue exitoso! Pipeline CI/CD ejecutándose correctamente.'
 
 if __name__ == '__main__':
-    # El puerto 5000 es el estándar para Flask
+    
     app.run(debug=True, host='0.0.0.0', port=5000)
